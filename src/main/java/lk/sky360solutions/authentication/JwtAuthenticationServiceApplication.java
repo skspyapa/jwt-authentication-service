ef8f128d11f63f05ce14b9a1b7a06bffec8cd5b0
skspyapa <supun@rumex.lk>
2021-06-02 20:51:57 +0530

    Add spring initializer project

---
+++ src/main/java/lk/sky360solutions/authentication/JwtAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.sky360solutions.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationServiceApplication.class, args);
+	}
+
+}
