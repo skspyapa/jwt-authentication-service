866abb0eb48fa98ab1cb5925925df7cf5399a7aa
skspyapa <supun@rumex.lk>
2021-07-14 02:22:24 +0530

    Remove unimported line

--- src/main/java/lk/sky360solutions/authentication/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/lk/sky360solutions/authentication/service/impl/RefreshTokenServiceImpl.java
@@ -11,7 +11,6 @@ import lk.sky360solutions.authentication.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.UUID;
 
